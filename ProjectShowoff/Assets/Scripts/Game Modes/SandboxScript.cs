8af7d1ab21f71fb3f8378b451a9362901c854ba4
Valentin <valio31032@gmail.com>
2021-05-10 11:31:58 +0200

    Added basis for game modes

---
+++ ProjectShowoff/Assets/Scripts/Game Modes/SandboxScript.cs
@@ -0,0 +1,15 @@
+
+public class SandboxScript : BaseGame
+{
+    //// Start is called before the first frame update
+    //protected override void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //protected override void Update()
+    //{
+        
+    //}
+}
