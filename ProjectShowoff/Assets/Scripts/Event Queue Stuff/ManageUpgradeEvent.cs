609d9b45e77dfdda2503c708e98289aa19b70c12
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-05-25 15:01:26 +0200

    ManageUpgradeEvent: spaces to tabs

--- ProjectShowoff/Assets/Scripts/Event Queue Stuff/ManageUpgradeEvent.cs
+++ ProjectShowoff/Assets/Scripts/Event Queue Stuff/ManageUpgradeEvent.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ManageUpgradeEvent : Event
 {
-    public Upgrade Upgrade => upgrade;
-    private Upgrade upgrade;
+	public Upgrade Upgrade => upgrade;
+	private Upgrade upgrade;
 
-    public ManageUpgradeEvent(Upgrade pUpgrade) : base(EventType.ManageUpgrade)
-    {
-        upgrade = pUpgrade;
-    }
+	public ManageUpgradeEvent(Upgrade pUpgrade) : base(EventType.ManageUpgrade)
+	{
+		upgrade = pUpgrade;
+	}
 }
