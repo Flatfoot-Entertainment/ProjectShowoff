02178886e31a6ea3275517a04a73e86a703da476
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-05-25 13:51:45 +0200

    ItemBoxData: Use inherited methods

--- ProjectShowoff/Assets/Scripts/Box/Data/ItemBoxData.cs
+++ ProjectShowoff/Assets/Scripts/Box/Data/ItemBoxData.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 public enum BoxType
 {
@@ -10,7 +8,6 @@ public enum BoxType
 	Type3
 }
 
-// TODO maybe call it BoxData or something?
 public class ItemBoxData : IBoxData<Item>
 {
 	public int MoneyValue => GetBoxContentsValue();
@@ -30,34 +27,6 @@ public class ItemBoxData : IBoxData<Item>
 		type = pBoxType;
 	}
 
-	public void AddItemToBox(Item item)
-	{
-		// TODO move to AddToBox
-		if (!boxContents.ContainsKey(item.Type)) //does the box already contain the item type?
-		{
-			boxContents.Add(item.Type, item.Value);
-		}
-		else
-		{
-			boxContents[item.Type] = boxContents[item.Type] + item.Value;
-		}
-		lookUp.Add(item);
-	}
-
-	public void RemoveItemFromBox(Item item)
-	{
-		// TODO move to RemoveFromBox
-		if (lookUp.Contains(item))
-		{
-			boxContents[item.Type] -= item.Value;
-			if (boxContents[item.Type] <= 0f)
-			{
-				boxContents.Remove(item.Type);
-			}
-			lookUp.Remove(item);
-		}
-	}
-
 	public int GetBoxContentsValue()
 	{
 		int contentsSum = 0;
@@ -70,11 +39,23 @@ public class ItemBoxData : IBoxData<Item>
 
 	public void AddToBox(Item contained)
 	{
-		AddItemToBox(contained);
+		if (!boxContents.ContainsKey(contained.Type))
+			boxContents.Add(contained.Type, contained.Value);
+		else
+			boxContents[contained.Type] = boxContents[contained.Type] + contained.Value;
+		lookUp.Add(contained);
 	}
 
 	public void RemoveFromBox(Item contained)
 	{
-		RemoveItemFromBox(contained);
+		if (lookUp.Contains(contained))
+		{
+			boxContents[contained.Type] -= contained.Value;
+			if (boxContents[contained.Type] <= 0f)
+			{
+				boxContents.Remove(contained.Type);
+			}
+			lookUp.Remove(contained);
+		}
 	}
 }
