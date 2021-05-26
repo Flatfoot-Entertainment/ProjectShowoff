7a164248c91def9ca3c79e934adfadaaa454390b
Valentin <valio31032@gmail.com>
2021-05-26 11:52:16 +0200

    BoxSelectionScript: clearer box selection + locking the buttons once selected

--- ProjectShowoff/Assets/Scripts/BoxSelectionScript.cs
+++ ProjectShowoff/Assets/Scripts/BoxSelectionScript.cs
@@ -6,25 +6,40 @@ using UnityEngine.UI;
 public class BoxSelectionScript : MonoBehaviour
 {
     [SerializeField] private Sprite[] boxImages;
+
+    [SerializeField] private Material transparentColor;
+
+    [SerializeField] private GameObject boxSelectionGroup;
+    [SerializeField] private GameObject[] boxSelectionModels;
     [SerializeField] private GameObject[] boxPrefabs;
     [SerializeField] private FulfillmentCenter fulfillmentCenter;
     [SerializeField] private Image placeholder;
-    private int imageIndex;
+    private int boxSelectionIndex;
 
     private void Start()
     {
+        foreach (GameObject gO in boxSelectionModels)
+        {
+            gO.GetComponentInChildren<MeshRenderer>().material = transparentColor;
+        }
         //EventScript.Instance.EventManager.Subscribe(EventType.ManageBoxSelect, ConfirmBox);
         fulfillmentCenter = FindObjectOfType<FulfillmentCenter>();
-        imageIndex = 0;
-        placeholder.sprite = boxImages[imageIndex];
+        boxSelectionIndex = 0;
+        boxSelectionModels[boxSelectionIndex].SetActive(true);
     }
 
     public void ChangeImage(int direction)
     {
-        imageIndex += direction;
-        if (imageIndex < 0) imageIndex = boxImages.Length - 1;
-        else if (imageIndex >= boxImages.Length) imageIndex = 0;
-        placeholder.sprite = boxImages[imageIndex];
+        int previousIndex = boxSelectionIndex;
+        boxSelectionIndex += direction;
+        if (boxSelectionIndex < 0) boxSelectionIndex = boxImages.Length - 1;
+        else if (boxSelectionIndex >= boxImages.Length) boxSelectionIndex = 0;
+
+        if (previousIndex != boxSelectionIndex)
+        {
+            boxSelectionModels[previousIndex].SetActive(false);
+        }
+        boxSelectionModels[boxSelectionIndex].SetActive(true);
     }
 
     public void ManageBoxConfirmation()
@@ -36,7 +51,8 @@ public class BoxSelectionScript : MonoBehaviour
         //TODO finish
         //ManageBoxSelectEvent boxSelectEvent = e as ManageBoxSelectEvent;
         //check if anything in threshold, if yes dont confirm, if no confirm
-        GameObject box = boxPrefabs[imageIndex];
+        GameObject box = boxPrefabs[boxSelectionIndex];
+        boxSelectionModels[boxSelectionIndex].SetActive(false);
         if (!box)
         {
             Debug.LogError("Yo this box don't exist");
@@ -44,7 +60,20 @@ public class BoxSelectionScript : MonoBehaviour
         }
         //rly not the best way but eh, probably turn to an event later on
         fulfillmentCenter.SpawnBox(box);
+        DisableBoxSelectionButtons();
     }
 
+    private void DisableBoxSelectionButtons()
+    {
+        Button[] boxButtons = boxSelectionGroup.GetComponentsInChildren<Button>();
+        foreach (Button boxButton in boxButtons) boxButton.interactable = false;
+
+    }
+
+    public void EnableBoxSelectionButtons()
+    {
+        Button[] boxButtons = boxSelectionGroup.GetComponentsInChildren<Button>();
+        foreach (Button boxButton in boxButtons) boxButton.interactable = true;
+    }
 
 }
