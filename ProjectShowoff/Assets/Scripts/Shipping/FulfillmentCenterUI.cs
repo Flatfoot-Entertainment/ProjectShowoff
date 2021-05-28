a68c86ca81a8b74ef12f72fb16b5719785371c38
Valentin <valio31032@gmail.com>
2021-05-28 14:42:38 +0200

    Added colliders to outer conveyor parts

--- ProjectShowoff/Assets/Scripts/Shipping/FulfillmentCenterUI.cs
+++ ProjectShowoff/Assets/Scripts/Shipping/FulfillmentCenterUI.cs
@@ -8,6 +8,9 @@ public class FulfillmentCenterUI : MonoBehaviour
     [SerializeField] private Button shipBoxButton;
     [SerializeField] private FulfillmentCenter fulfillmentCenter;
 
+    private void Start() {
+        fulfillmentCenter = FindObjectOfType<FulfillmentCenter>();
+    }
     private void Update()
     {
         //change so it doesnt update every frame
