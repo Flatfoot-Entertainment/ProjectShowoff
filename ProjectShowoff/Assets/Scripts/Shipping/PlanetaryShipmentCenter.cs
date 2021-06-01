b85ef0cbb74fc0954d05b93d51776b6a5d6aca6f
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2021-06-01 16:16:12 +0200

    PlanetaryShipmentCenter: Disable button while shipping

--- ProjectShowoff/Assets/Scripts/Shipping/PlanetaryShipmentCenter.cs
+++ ProjectShowoff/Assets/Scripts/Shipping/PlanetaryShipmentCenter.cs
@@ -1,11 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlanetaryShipmentCenter : MonoBehaviour
 {
 	private Planet selectedPlanet;
 	[SerializeField] private ShipComponents ship;
+	[SerializeField] private Button shipButton;
+
+	private void Start()
+	{
+		ship.Ship.OnArrival += OnShipReturned;
+		shipButton.interactable = true;
+	}
 
 	public void DeliverSelected()
 	{
@@ -15,6 +21,7 @@ public class PlanetaryShipmentCenter : MonoBehaviour
 		// Instantiate a ship
 		ship.LoadingHandler.Ship();
 		ship.Ship.DeliverTo(selectedPlanet);
+		shipButton.interactable = false;
 
 		selectedPlanet.Deselect();
 
@@ -27,9 +34,15 @@ public class PlanetaryShipmentCenter : MonoBehaviour
 		selectedPlanet = planet;
 		selectedPlanet.Select();
 	}
+	
 	public void DeselectPlanet()
 	{
 		if (selectedPlanet) selectedPlanet.Deselect();
 		selectedPlanet = null;
+	}
+
+	private void OnShipReturned()
+	{
+		shipButton.interactable = true;
 	}
 }
